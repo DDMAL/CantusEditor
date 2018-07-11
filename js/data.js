3b8bdd6696164f62d5c7c619d13accd54cf6a40c
Nestor Napoles <napulen@gmail.com>
2018-07-11 13:52:26 -0400

    New: Separating the js code in two files

---
+++ js/data.js
@@ -0,0 +1,34 @@
+(function () {
+    "use strict";
+    document.manuscripts = {
+        "CSG-0390-ID": {
+            name: "St. Gallen, CSG-390",
+            description: "Winter volume of the so-called Hartker Antiphonary",
+            meiId: "csg-0390",
+            meiSource: "local",
+            iiifId: "csg-0390",
+            iiifSource: "e_codices"
+        }
+    };
+    document.meiSources = {
+        local: {
+            name: "Local folder",
+            description: "Files stored within the CantusEditor",
+            baseURL: document.URL,
+            getMeiURL: function (meiId) {
+                return this.baseURL + "resources/mei/" + meiId + "/";
+            }
+        }
+    };
+    document.iiifSources = {
+        e_codices: {
+            name: "Virtual Manuscript Library of Switzerland",
+            description: "A free-access virtual library to all medieval" +
+                    " and a selection of modern manuscripts of Switzerland",
+            baseURL: "https://www.e-codices.unifr.ch/",
+            getManifestURL: function (iiifId) {
+                return this.baseURL + "metadata/iiif/" + iiifId + "/manifest.json";
+            }
+        }
+    };
+}());
